c2582e8d60dfb208dd665c928df688add794cd97
Thomas Bnt <thomasbnt@protonmail.com>
2021-05-21 17:16:32 +0200

    Better dimensions of badges. Close #7

--- Badges.js
+++ Badges.js
@@ -1,54 +1,58 @@
 module.exports = {
     BOT: {
         name: "Bot",
-        url: "https://cdn.discordapp.com/emojis/707103051437178921.png?v=1"
+        url: "/img/badges/bot.png"
     },
     BUGHUNTER_LEVEL_1: {
         name: "Discord Bug Hunter",
-        url: "https://cdn.discordapp.com/emojis/604997907095093248.png?v=1"
+        url: "/img/badges/bug_hunter.png"
     },
     BUGHUNTER_LEVEL_2: {
         name: "Discord Bug Hunter",
-        url: "https://cdn.discordapp.com/emojis/657002233556107264.png?v=1"
+        url: "/img/badges/bug_hunter_2.png"
     },
     DISCORD_EMPLOYEE: {
         name: "Discord Staff",
-        url: "https://cdn.discordapp.com/emojis/604997907397214208.png?v=1"
+        url: "/img/badges/staff.png"
     },
     DISCORD_NITRO: {
         name: "Discord Nitro",
-        url: "https://cdn.discordapp.com/emojis/314068430611415041.png?v=1"
+        url: "/img/badges/nitro.png"
     },
     DISCORD_PARTNER: {
         name: "Discord Partner",
-        url: "https://cdn.discordapp.com/emojis/728201573527978036.png?v=1"
+        url: "/img/badges/partner.png"
     },
     EARLY_SUPPORTER: {
         name: "Early Supporter",
-        url: "https://cdn.discordapp.com/emojis/720584849832017920.png?v=1"
+        url: "/img/badges/early_supporter.png"
     },
     HOUSE_BALANCE: {
         name: "HypeSquad Balance",
-        url: "https://cdn.discordapp.com/emojis/640336405431713802.png?v=1"
+        url: "/img/badges/hypesquad_balance.png"
     },
     HOUSE_BRAVERY: {
         name: "HypeSquad Bravery",
-        url: "https://cdn.discordapp.com/emojis/640336405079392319.png?v=1"
+        url: "/img/badges/hypesquad_bravery.png"
     },
     HOUSE_BRILLIANCE: {
         name: "HypeSquad Brilliance",
-        url: "https://cdn.discordapp.com/emojis/640336405377187842.png?v=1"
+        url: "/img/badges/hypesquad_brilliance.png"
     },
     HYPESQUAD_EVENTS: {
         name: "HypeSquad Events",
-        url: "https://cdn.discordapp.com/emojis/604997907053281300.png?v=1"
+        url: "/img/badges/hypesquad_events.png"
     },
     VERIFIED_BOT: {
         name: "Verified Bot",
-        url: "https://cdn.discordapp.com/emojis/730842910383407155.png?v=1"
+        url: "/img/badges/verified_bot.png"
     },
     VERIFIED_DEVELOPER: {
         name: "Verified Bot Developer",
-        url: "https://cdn.discordapp.com/emojis/720584852063387719.png?v=1"
+        url: "/img/badges/verified_bot_developer.png"
+    },
+    CERTIFIED_MODERATOR: {
+        name: "Certified Moderator",
+        url: "/img/badges/certified_moderator.png"
     }
 };
