2387d84263983350759da4252f0e0d868ceee203
Thomas Bonnet <thomasbnt@protonmail.com>
2025-07-08 02:59:09 +0200

    fix(app.js): Prevent duplicate ACTIVE_DEVELOPER badge assignment, Close #17

--- app.js
+++ app.js
@@ -51,7 +51,9 @@ app.get(`/:userID`, async (req, res) => {
         if (user.avatar && user.avatar.startsWith("a_")) flags.push(Badges["DISCORD_NITRO"])
         if (user.flags.has(1 << 18)) flags.push(Badges["CERTIFIED_MODERATOR"])
         if (user.flags.has(1 << 17)) flags.push(Badges["VERIFIED_DEVELOPER"])
-        if (user.flags.has(1 << 22)) flags.push(Badges["ACTIVE_DEVELOPER"])
+        if (user.flags.has(1 << 22) && !flags.includes(Badges["ACTIVE_DEVELOPER"])) {
+            flags.push(Badges["ACTIVE_DEVELOPER"])
+        }
         if (user.bot) {
             flags.push(Badges["BOT"])
         }
